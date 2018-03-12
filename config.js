f83c4d040061cf894621ab1ac1608bba65e4a47f
MJ <kipsang20323@gmail.com>
2018-03-12 17:25:14 -0600

    add secure cmd string to travis

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://goosy12:goose7681@ds213199.mlab.com:13199/mongoose-blog-ci';
 exports.PORT = process.env.PORT || 8080;
